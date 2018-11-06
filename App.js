94b6c8759e7d41a387454384a3e7cf9b2b7883a0
Janus <gudjon15@ru.is>
2018-11-06 21:35:53 +0000

    purdifying Home.js

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React from 'react'
+import { Platform, StatusBar, StyleSheet, View } from 'react-native'
+import { AppLoading, Asset, Font, Icon } from 'expo'
+import AppNavigator from './navigation/AppNavigator'
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers/reducers';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers/reducers'
 
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false,
-  };
+    isLoadingComplete: false
+  }
 
-  render() {
+  render () {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
         <AppLoading
@@ -21,16 +21,16 @@ export default class App extends React.Component {
           onError={this._handleLoadingError}
           onFinish={this._handleFinishLoading}
         />
-      );
+      )
     } else {
       return (
         <Provider store={createStore(reducers, applyMiddleware(thunk))}>
           <View style={styles.container}>
-            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+            {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
             <AppNavigator />
           </View>
         </Provider>
-      );
+      )
     }
   }
 
@@ -38,32 +38,32 @@ export default class App extends React.Component {
     return Promise.all([
       Asset.loadAsync([
         require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
+        require('./assets/images/robot-prod.png')
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+      })
+    ])
+  }
 
   _handleLoadingError = error => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
-    console.warn(error);
-  };
+    console.warn(error)
+  }
 
   _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
-  };
+    this.setState({ isLoadingComplete: true })
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+    backgroundColor: '#fff'
+  }
+})
