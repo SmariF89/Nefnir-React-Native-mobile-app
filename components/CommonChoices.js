54fd9776318a6811abc8c994496945e2152f1767
Janus <gudjon15@ru.is>
2018-11-07 21:25:07 +0000

    add style to commonChoises

--- components/CommonChoices.js
+++ components/CommonChoices.js
@@ -1,101 +1,64 @@
-import React from 'react';
+import React from 'react'
 import {
-	View,
-	Text,
-	TouchableOpacity,
-	StyleSheet,
-	FlatList
-} from 'react-native';
-import { connect } from 'react-redux';
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    FlatList
+} from 'react-native'
+import { connect } from 'react-redux'
+
+import styles from '../styles/styles'
 
 class CommonChoices extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor (props) {
+    super(props)
 
-		this.state = {
-			choices: []
-		};
-	}
+    this.state = {
+      choices: []
+    }
+  }
 
-	componentDidMount() {
-		this.setState({ choices: this.props.data.choice.commonChoices });
-	}
+  componentDidMount () {
+    this.setState({ choices: this.props.data.choice.commonChoices })
+  }
 
-	render() {
-		const { choices } = this.state;
-		console.log(choices);
-		return (
-			<View>
-				<Text style={styles.TitleToCommonChoices}>
-					Here are the names you both agreed on
-				</Text>
-				<View>
-					<FlatList
-						data={choices}
-						renderItem={({ item }) => (
-							<Text style={styles.commonChoiceText}>{item}</Text>
-						)}
-					/>
-				</View>
-
-				<TouchableOpacity
-					style={styles.btn}
-					activeOpacity={0.5}
-					onPress={() => this.props.navigation.goBack(null)}>
-					<Text style={styles.btnText}>Go back</Text>
-				</TouchableOpacity>
-			</View>
-		);
-	}
+  render () {
+    const { choices } = this.state
+    console.log(choices)
+    return (
+      <View style={styles.container}>
+        <View style={styles.logoContainer}>
+          <Text style={styles.textAlignCenterBig}>
+                        Here are the names you both agreed on
+                    </Text>
+        </View>
+        <View style={styles.commonContainer}>
+          <FlatList
+            data={choices}
+            renderItem={({ item }) => (
+              <Text style={styles.textAlignLeft}>{item}</Text>
+                        )}
+                    />
+        </View>
+        <View style={styles.aboutContainer}>
+          <TouchableOpacity
+            style={styles.btn}
+            activeOpacity={0.5}
+            onPress={() => this.props.navigation.goBack(null)}
+                    >
+            <Text style={styles.btnText}>Go back</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		data: state
-	};
-};
+  return {
+    data: state
+  }
+}
 
-const styles = StyleSheet.create({
-	btn: {
-		marginTop: 10,
-		marginBottom: 15,
-		marginLeft: 70,
-		marginRight: 70,
-		paddingTop: 10,
-		paddingBottom: 10,
-		borderWidth: 1,
-		borderRadius: 10,
-		borderColor: 'gray',
-		backgroundColor: '#649cef'
-	},
-	btnText: {
-		textAlign: 'center',
-		fontSize: 18,
-		color: 'white'
-	},
-	commonChoiceText: {
-		fontSize: 28,
-		textAlign: 'center',
-		marginBottom: 5,
-		borderWidth: 1,
-		borderRadius: 2,
-		borderColor: '#ddd',
-		borderBottomWidth: 0,
-		shadowColor: '#000',
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.8,
-		shadowRadius: 2
-	},
-	TitleToCommonChoices: {
-		fontSize: 28,
-		textAlign: 'center',
-		marginBottom: 40,
-		color: 'white',
-		backgroundColor: '#649cef'
-	}
-});
-
-export default connect(
-	mapStateToProps,
-	null
-)(CommonChoices);
+export default connect(mapStateToProps, null)(CommonChoices)
